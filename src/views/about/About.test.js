79d7c7e9ee9e1983dff44f209e259b07d50ce947
Justin Shilling <loveofmusic25@hotmail.com>
2021-06-06 11:15:31 -0400

    removed unused files from template

--- src/views/about/About.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import About from "./About";
-
-test("renders", () => {
-	render(<About />);
-	// const linkElement = screen.getByText(/learn react/i);
-	// expect(linkElement).toBeInTheDocument();
-});
